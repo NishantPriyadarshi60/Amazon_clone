345cc4da127abf2e6ed0922a1618cf562ddaa21c
priyadarshiNishant <priyadarshinishant063@gmail.com>
2024-02-09 22:48:52 +0530

    AMAZON_CLONE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './Home';
+import Checkout from './Checkout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <Routes>
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reducer, { initialState } from "./reducer";
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from "./StateProvider";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}></StateProvider>
     <App />
   </React.StrictMode>
 );
